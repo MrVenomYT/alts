3c8e1861251b787fbaf66ab1d2813da764e666ba
Muhammad Hasil <t3ch91cal.h3r0@gmail.com>
2023-08-11 01:29:25 +0500

    Update index.js

--- index.js
+++ index.js
@@ -1,4 +1,4 @@
 // how do i write javascript
 console.log("opening the console is user error");
 debugger;
-window.location.href = "https://www.minecraft.net/store/minecraft-java-bedrock-edition-pc";
+window.location.href = "https://alts-90dfe71su-mrvenomyt.vercel.app/";
